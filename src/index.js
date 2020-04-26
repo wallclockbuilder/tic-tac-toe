de14cadad8a483a63b1fee3b4459df9ae3bf6c78
Mawueli Adzoe <wallclockbuilder@googlemail.com>
2020-04-26 16:10:19 -0700

    Ignore play if there is a winner or if square is already filled.

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ function Square(props) {
 
     handleClick(i){
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
